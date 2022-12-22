97a1abaf60b2ed9769132d162e0fe0bed726fdda
henri <henri@LAPTOP-8OV37SD4>
2022-12-21 21:33:50 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+public static class Program
+{
+    static void Main(string[] args)
+    {
+        static int funcaoRetornaQuantitadePOV(decimal porcentagem, int totalNegociado) => (int)(totalNegociado / (1 - porcentagem) * porcentagem);
+    }
+}
